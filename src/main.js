016335c3df7d23b8b2f6f319cb17a5ca05780a3b
Ljoskold <128295295+Ljoskold@users.noreply.github.com>
2024-04-05 17:24:13 +0700

    basic responsiveness achieved

---
+++ src/main.js
@@ -0,0 +1,2 @@
+import styles from './styles.css';
+import '@fortawesome/fontawesome-free/css/all.css'; // Ensure the correct path to your Font Awesome CSS
---
+++ webpack.config.js
@@ -0,0 +1,58 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+	mode: 'development',
+	entry: './src/main.js',
+	output: {
+		filename: 'bundle.js',
+		path: path.resolve(__dirname, 'dist'),
+	},
+	watchOptions: {
+		ignored: /\.(mp3|ogg|wav)$/,
+	},
+	module: {
+		rules: [
+			{
+				test: /\.(ogg|mp3|wav)$/i,
+				use: [
+					{
+						loader: 'file-loader',
+						options: {
+							name: '[name].[ext]',
+							outputPath: 'audio/',
+							publicPath: 'audio/',
+						},
+					},
+				],
+			},
+			{
+				test: /\.css$/,
+				use: ['style-loader', 'css-loader'],
+			},
+			{
+				test: /\.(png|jpg|jpeg|gif|svg)$/i,
+				type: 'asset/resource',
+				generator: {
+					filename: 'images/[name][ext]',
+				},
+			},
+			{
+				test: /\.(woff|woff2|eot|ttf|otf)$/i,
+				type: 'asset/resource',
+				generator: {
+					filename: 'fonts/[name][ext]',
+				},
+			},
+		],
+	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			template: './src/index.html',
+			filename: 'index.html',
+		}),
+	],
+	resolve: {
+		extensions: ['.js', '.mjs'],
+	},
+};
